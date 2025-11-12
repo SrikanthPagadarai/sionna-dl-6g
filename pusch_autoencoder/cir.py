76598356d95c77d784d7abca4276687713ae31de
Srikanth <srikanth.pagadarai@analog.com>
2025-11-12 13:07:24 -0600

    autoencoder: split cir.py to move cir generation functionality to a different class
    Signed-off-by: Srikanth <srikanth.pagadarai@analog.com>

--- pusch_autoencoder/cir.py
+++ pusch_autoencoder/cir.py
@@ -47,6 +47,7 @@ import matplotlib.pyplot as plt
 # New: central configuration
 # ----------------------------
 from config import Config
+from cir_generator import CIRGenerator
 _cfg = Config()
 
 # system parameters (read from config to avoid functionality changes)
@@ -248,47 +249,6 @@ tau = tau[p_link>0,...]
 print('5)')
 print('a.shape: ', a.shape)
 print('tau.shape: ', tau.shape)
-
-class CIRGenerator:
-    """(Unchanged)"""
-
-    def __init__(self,
-                 a,
-                 tau,
-                 num_tx):
-
-        # Copy to tensorflow
-        self._a = tf.constant(a, tf.complex64)
-        self._tau = tf.constant(tau, tf.float32)
-        self._dataset_size = self._a.shape[0]
-
-        self._num_tx = num_tx
-
-    def __call__(self):
-
-        # Generator implements an infinite loop that yields new random samples
-        while True:
-            # Sample 4 random users and stack them together
-            idx,_,_ = tf.random.uniform_candidate_sampler(
-                            tf.expand_dims(tf.range(self._dataset_size, dtype=tf.int64), axis=0),
-                            num_true=self._dataset_size,
-                            num_sampled=self._num_tx,
-                            unique=True,
-                            range_max=self._dataset_size)
-
-            a = tf.gather(self._a, idx)
-            tau = tf.gather(self._tau, idx)
-
-            # Transpose to remove batch dimension
-            a = tf.transpose(a, (3,1,2,0,4,5,6))
-            tau = tf.transpose(tau, (2,1,0,3))
-
-            # And remove batch-dimension
-            a = tf.squeeze(a, axis=0)
-            tau = tf.squeeze(tau, axis=0)
-
-            yield a, tau
-
 
 batch_size = _cfg.BATCH_SIZE  # Must equal CIRDataset batch size for BER sims
 
---
+++ pusch_autoencoder/cir_generator.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+
+class CIRGenerator:
+    """(Unchanged)"""
+
+    def __init__(self,
+                 a,
+                 tau,
+                 num_tx):
+
+        # Copy to tensorflow
+        self._a = tf.constant(a, tf.complex64)
+        self._tau = tf.constant(tau, tf.float32)
+        self._dataset_size = self._a.shape[0]
+
+        self._num_tx = num_tx
+
+    def __call__(self):
+
+        # Generator implements an infinite loop that yields new random samples
+        while True:
+            # Sample 4 random users and stack them together
+            idx,_,_ = tf.random.uniform_candidate_sampler(
+                            tf.expand_dims(tf.range(self._dataset_size, dtype=tf.int64), axis=0),
+                            num_true=self._dataset_size,
+                            num_sampled=self._num_tx,
+                            unique=True,
+                            range_max=self._dataset_size)
+
+            a = tf.gather(self._a, idx)
+            tau = tf.gather(self._tau, idx)
+
+            # Transpose to remove batch dimension
+            a = tf.transpose(a, (3,1,2,0,4,5,6))
+            tau = tf.transpose(tau, (2,1,0,3))
+
+            # And remove batch-dimension
+            a = tf.squeeze(a, axis=0)
+            tau = tf.squeeze(tau, axis=0)
+
+            yield a, tau
